f572af31f5b8d0a602eefb9f4aba8daa73c9ca80
fellipetereska <fellipetereska@gmail.com>
2025-04-07 21:43:38 -0300

    att

--- app/src/main/java/com/example/userregistrationapp/MainActivity.java
+++ app/src/main/java/com/example/userregistrationapp/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.userregistrationapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.*;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
+
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextCPF, editTextEmail, editTextPhone;
+
+    private UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); // Define o layout da tela
+
+        //Inicializar os campos de entrada
+        editTextName = findViewById(R.id.editTextName);
+        editTextCPF = findViewById(R.id.editTextCPF);
+        editTextEmail = findViewById(R.id.editTextMail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        // Inicializa os botões
+        Button buttonSave = findViewById(R.id.buttonSave);
+        Button buttonReport = findViewById(R.id.buttonReport);
+
+        // Configuração do banco de dados usando o Room
+        UserDataBase db = Room.databaseBuilder(getApplicationContext(),
+                userDatabase.class, "user-database").allowMainThreadQueries().build();
+        userDao - db.userDao(); // Obtem uma instancia DAO para interagir com os dados
+
+
+        // Configura o botao de salvar
+        buttonSave.setOnClickListener(v -> {
+            Log.d("MainActivity", "Botão Cadastrar Usuário clicado");
+
+            // Coleta os dados do front
+            string name = editTextName.getText().toString();
+            string cpf = editTextCPF.getText().toString();
+            string email = editTextEmail.getText().toString();
+            string phone = editTextPhone.getText().toString();
         });
     }
 }
